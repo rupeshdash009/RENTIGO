33dec07ea969367caa974f5f2febbf992fc78a57
Rupesh Dash <rupeshdash20@gmail.com>
2026-06-08 22:36:16 +0530

    db changes isses

--- BACK-END/config/db.js
+++ BACK-END/config/db.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
-const dns = require("dns");
-const { connected } = require("process");
-
-// Force Node.js to use public DNS for MongoDB SRV lookup
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is missing in environment variables");
+    }
+
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`MongoDB Connected: Connected`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`Database Name: ${conn.connection.name}`);
   } catch (error) {
     console.error(`MongoDB connection failed: ${error.message}`);
-    throw error;
+    process.exit(1);
   }
 };
 
